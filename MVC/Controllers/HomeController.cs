e5ae789551ca06baf54041008a80d64ee1b9c3a7
Jeremy Brammer <jbrammer03@gmail.com>
2017-06-22 10:02:18 -0500

    Added a custom authorization attribute.

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using MVC.custom_classes;
+
 namespace MVC.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Home/protected_page
+        [AuthenticatedUser("admin")]
+        public ActionResult protected_page()
         {
             return View();
         }
---
+++ MVC/custom_classes/AuthenticatedUserAttribute.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVC.custom_classes
+{
+    public class AuthenticatedUserAttribute : AuthorizeAttribute
+    {
+        private string[] allowedRoles { get; set; }
+
+        public AuthenticatedUserAttribute() : base()
+        {
+        }
+
+        //Overloaded constructor to take arguments for allowed roles:
+        public AuthenticatedUserAttribute(params string[] allowedRoles) : base()
+        {
+            this.allowedRoles = allowedRoles;
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool authorize = false;
+            //Check if the 'Authenticated' session boolean is true:
+            if ((bool)httpContext.Session["Authenticated"])
+            {
+                //Check if the session RoleName is in the list of allowed roles:
+                foreach(string role in allowedRoles){
+                    if(role == httpContext.Session["RoleName"].ToString()){
+                        authorize = true;
+                    }
+                }
+            }
+            return authorize;
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            //Later this needs to redirect to a page based on user role:
+            RouteValueDictionary route = new RouteValueDictionary(){
+                { "action", "Index" },
+                { "controller", "Home" }
+            };
+            filterContext.Result = new RedirectToRouteResult(route);
+                                   
+        }  
+    }
+}
