a3b938ab41c73cd7bde2141518f86c4e1b05d5b9
Jeremy Brammer <jbrammer03@gmail.com>
2017-07-28 09:29:55 -0500

    Added logic for "update movie" controller in MVC app.

--- MVC/Controllers/AdminController.cs
+++ MVC/Controllers/AdminController.cs
@@ -6,16 +6,37 @@ using System.Web.Mvc;
 
 using MVC.custom_classes;
 using MVC.Models.AdminViewModels;
+using SharedResources.Interfaces;
+using SharedResources.Mappers;
+using CompositionRoot;
+using SharedResources.Exceptions.BLL;
+using MVC.Models;
 
 namespace MVC.Controllers
 {
     public class AdminController : Controller
     {
+        IMoviesBLL movies_bll { get; set; }
+
+        public AdminController()
+        {
+            CRoot CompositionRoot = new CRoot("prod");
+            movies_bll = CompositionRoot.MoviesBLL; 
+        }
+
+
         // GET: Admin
         [AuthenticatedUser("admin")]
         public ActionResult Index()
         {
-            return View();
+            AddMovieViewModel viewModel = new AddMovieViewModel();
+
+            if (TempData["ErrorMessage"] != null)
+            {
+                viewModel.errorMessage = TempData["ErrorMessage"].ToString();
+            }
+
+            return View(viewModel);
         }
 
         // GET: Admin/AddMovie
@@ -29,7 +50,27 @@ namespace MVC.Controllers
         [AuthenticatedUser("admin")]
         public ActionResult AddMovie(AddMovieViewModel viewModel)
         {
-            return View(viewModel);
+            //Declare redirect path variables:
+            string redirect_action;
+            string redirect_controller;
+
+            try
+            {
+                movies_bll.InsertMovie(new MovieMapper { Title = viewModel.Title, RunTime = viewModel.RunTime, Image = viewModel.Image });
+                //redirect path variables:
+                redirect_action = "index";
+                redirect_controller = "Admin";
+            }
+            catch (BLLException)
+            {
+                //redirect path variables:
+                redirect_action = "index";
+                redirect_controller = "Admin";
+                TempData["ErrorMessage"] = "Something went wrong.  Please try again.";
+            }
+            //Redirect to the dashboard (protected page):
+            return RedirectToAction(redirect_action, redirect_controller);
+            //return View(viewModel);
         }
 
 
